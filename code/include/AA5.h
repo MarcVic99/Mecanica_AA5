d9813c6d5ec5638f4168517f76f6aeeed182ef05
MarcVIc <marc.vicente@enti.cat>
2022-05-26 15:54:40 +0200

    Cloth render and first steps

--- code/include/AA5.h
+++ code/include/AA5.h
@@ -3,17 +3,31 @@
 #include <vector>
 #include <glm/glm.hpp>
 
+extern bool renderParticles;
+extern bool renderSphere;
+extern bool renderCloth;
+
+namespace ClothMesh
+{
+	extern void updateClothMesh(float* aray_data);
+	extern const int numCols;
+	extern const int numRows;
+}
 namespace AA5 
 {
+	
 	class Wave 
 	{
 	public:
 		glm::vec3 GetPositionAtTime(glm::vec3 initialPosition, float time);
-
+	
 	private:
 		glm::vec3 direction;
 		float amplitude;
-		float wavelength;
+		float waveLength;
+
+		float frequency;
+		float fase;
 	};
 
 	class Sphere 
@@ -24,9 +38,31 @@ namespace AA5
 		float density;
 	};
 
+	class MeshTest
+	{
+	public:
+		MeshTest();
+		~MeshTest();
+
+		int GetIndex(int col, int row);
+
+		void RenderUpdateMesh();
+
+		void Update();
+
+		void SetInitialMeshPosition();
+		void SetInitialMeshParticlePositionAndVelocity();
+
+		glm::vec3* positions;
+
+
+	};
+
 	class FluidSimulator : public Simulator 
 	{
 	public:
+		FluidSimulator();
+		~FluidSimulator();
 		void Update(float dt);
 		void RenderUpdate();
 		void RenderGui();
@@ -37,5 +73,10 @@ namespace AA5
 		std::vector<Wave> waves;
 		std::vector<glm::vec3> positions; // 18 x 14 mesh
 		Sphere sphere;
+		MeshTest *meshTest;
 	};
+
+	
+
+
 }
--- code/src/AA5.cpp
+++ code/src/AA5.cpp
@@ -1,25 +1,103 @@
 #include "AA5.h"
 
-// == WAVE ==
-glm::vec3 AA5::Wave::GetPositionAtTime(glm::vec3 initialPosition, float time)
-{
-	return glm::vec3();
+namespace AA5 {
+	FluidSimulator::FluidSimulator()
+	{
+		renderParticles = false;
+		renderCloth = true;
+		renderSphere = false;
+
+		meshTest = new MeshTest();
+	}
+
+	FluidSimulator::~FluidSimulator()
+	{
+		renderCloth = false;
+	}
+
+	// == WAVE ==
+	glm::vec3 AA5::Wave::GetPositionAtTime(glm::vec3 initialPosition, float time)
+	{
+		glm::vec3 resultingPosition;
+		glm::vec3 waveVectorK;
+		float offsetY = 5.f;
+
+		// k = 2 * pi / lambda
+		waveVectorK = direction * glm::vec3(2.f * 3.14f / waveLength);
+
+		// x = x_0 - summation:(K_i/k_i) * A_i * sin(k_i * x_0 - w_i * t + phi_i)
+		resultingPosition.x = initialPosition.x - direction.x * amplitude * sin(waveVectorK.x * initialPosition.x - frequency * time + fase);
+
+		// y = offsetY + summation:A_i * cos(K_i * x_0 - w_i * t + phi_i)
+		resultingPosition.y = offsetY + amplitude * cos(waveVectorK.y * initialPosition.y - frequency * time + fase);
+
+		return resultingPosition;
+	}
+
+	// == FLUID SIMULATOR ==
+	void FluidSimulator::Update(float dt)
+	{
+	}
+
+	void FluidSimulator::RenderUpdate()
+	{
+		meshTest->RenderUpdateMesh();
+	}
+
+	void FluidSimulator::RenderGui()
+	{
+	}
+
+	float FluidSimulator::GetSphereSubmergedVolume()
+	{
+		return 0.0f;
+	}
+
+
+	MeshTest::MeshTest()
+	{
+		positions = new glm::vec3[ClothMesh::numCols * ClothMesh::numRows];
+		SetInitialMeshPosition();
+	}
+
+	MeshTest::~MeshTest()
+	{
+		renderCloth = false;
+	}
+
+	int MeshTest::GetIndex(int col, int row)
+	{
+		return col + (row * ClothMesh::numCols);
+	}
+
+	void MeshTest::Update()
+	{
+		for (float row = 0; row < ClothMesh::numRows; row++)
+		{
+			for (float col = 0; col < ClothMesh::numCols; col++)
+			{
+				int indx = GetIndex(col, row);
+			}
+		}
+	}
+
+	void MeshTest::RenderUpdateMesh()
+	{
+		ClothMesh::updateClothMesh(&(positions[0].x));
+	}
+
+	void MeshTest::SetInitialMeshPosition()
+	{
+		for (float row = 0; row < ClothMesh::numRows; row++)
+		{
+			for (float col = 0; col < ClothMesh::numCols; col++)
+			{
+				int indx = GetIndex(col, row);
+				positions[indx] = glm::vec3(((10.f/ClothMesh::numRows) * row) - 5, 0, ((10.f / ClothMesh::numCols) * col) - 5.f);
+				positions[indx] = positions[indx];
+			}
+		}
+	}
+
 }
 
-// == FLUID SIMULATOR ==
-void AA5::FluidSimulator::Update(float dt)
-{
-}
-
-void AA5::FluidSimulator::RenderUpdate()
-{
-}
-
-void AA5::FluidSimulator::RenderGui()
-{
-}
-
-float AA5::FluidSimulator::GetSphereSubmergedVolume()
-{
-	return 0.0f;
-}
